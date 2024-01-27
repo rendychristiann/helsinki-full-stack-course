7e807a93db91a7c32501b35c5aa69a485786d041
Rendy Christian <rendychristiann@gmail.com>
2024-01-27 18:30:45 +0700

    update part 2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newFilter, setNewFilter] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     personService.getAll().then((initialPersons) => {
@@ -20,7 +21,10 @@ const App = () => {
       item.name.includes(newName)
     );
     if (filterDuplicate.length > 0) {
-      alert(`${newName} is already added to phonebook`);
+      setErrorMessage(`${newName} is already added to phonebook`)
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
     } else {
       const personObject = {
         name: newName,
@@ -31,6 +35,10 @@ const App = () => {
         setPersons(persons.concat(returnedPerson));
         setNewName("");
         setNewNumber("");
+        setErrorMessage(`Added ${newName} to phonebook`)
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
       });
       console.log(persons);
     }
@@ -47,6 +55,10 @@ const App = () => {
       personService.remove(id).then((response) => {
         console.log(response);
         setPersons(persons.filter(person => person.id !== response.id));
+        setErrorMessage(`${findDelete.name} was deleted from the phonebook`)
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
       })
     }
 
@@ -68,9 +80,10 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h2 className="text-teal-500 font-bold text-3xl pt-5 ml-5">Phonebook</h2>
+      <Notification message={errorMessage} />
       <Filter newFilter={newFilter} setNewFilter={setNewFilter} />
-      <h3>Add a new</h3>
+      <h3 className="text-teal-500 font-bold text-3xl ml-5">Add a new</h3>
       <PersonForm
         addPersons={addPersons}
         setNewName={setNewName}
@@ -78,7 +91,7 @@ const App = () => {
         newName={newName}
         newNumber={newNumber}
       />
-      <h2>Numbers</h2>
+      <h2 className="text-teal-500 font-bold text-3xl py-5 ml-5">Numbers</h2>
       <Persons persons={filterValue} toggleImportanceOf={toggleImportanceOf} />
     </div>
   );
@@ -86,10 +99,17 @@ const App = () => {
 
 const Display = ({ name, number, onClick }) => {
   return (
-    <p>
-      {name} {number}
-      <button onClick={onClick}>Delete</button>
-    </p>
+    <div className="relative overflow-x-auto">
+      <table className="w-1/2 text-sm text-left rtl:text-right">
+        <tbody>
+          <tr>
+            <th scope="row" className="pl-5 whitespace-nowrap font-bold text-teal-500">{name}</th>
+            <td className="font-semibold ">{number}</td>
+            <td><button onClick={onClick} className="mx-5 my-2 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-1 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Delete</button></td>
+          </tr>
+        </tbody>
+      </table> 
+    </div>
   );
 };
 
@@ -112,12 +132,14 @@ const Persons = ({ persons, toggleImportanceOf }) => {
 
 const Filter = ({ newFilter, setNewFilter }) => {
   return (
-    <div>
-      filter shown with{" "}
-      <input
+    <div >
+      <p className="pt-5 ml-5 font-semibold text-xl">Filter shown with{" "}</p>
+      <div>
+      <input className="mx-5 my-5 shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Search phonebook"
         value={newFilter}
         onChange={(event) => setNewFilter(event.target.value)}
       />
+      </div>
     </div>
   );
 };
@@ -132,24 +154,35 @@ const PersonForm = ({
   return (
     <form onSubmit={addPersons}>
       <div>
-        name:{" "}
-        <input
+        <p className="pt-2 ml-5 font-normal text-md">Name:</p>
+        <input className="mx-5 my-2 shadow appearance-none border rounded w-1/2 py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="name"
           value={newName}
           onChange={(event) => setNewName(event.target.value)}
         />
       </div>
       <div>
-        number:{" "}
-        <input
+        <p className="ml-5 font-normal text-md">Number:</p>
+        <input  className="mx-5 my-2 shadow appearance-none border rounded w-1/2 py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="phone number"
           value={newNumber}
           onChange={(event) => setNewNumber(event.target.value)}
         />
       </div>
       <div>
-        <button type="submit">add</button>
+        <button type="submit" className="mx-5 my-2 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">add</button>
       </div>
     </form>
   );
 };
+
+const Notification = ({message}) => {
+  if (message === null){
+    return null
+  }
+  return(
+    <div className="text-black bg-slate-300 text-xl font-bold mx-5 my-5 px-5 py-5 border border-solid rounded-lg w-2/3 border-red-500">
+      {message}
+    </div>
+  )
+}
 
 export default App;
